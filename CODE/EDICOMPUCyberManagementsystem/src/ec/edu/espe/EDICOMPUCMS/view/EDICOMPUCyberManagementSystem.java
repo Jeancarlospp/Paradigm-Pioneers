39f5679b22f901657e831ac2b126c803fb839031
Jeancarlo Javier Santi Cruz <sjeancarlo.05jjsc@gmail.com>
2024-06-09 20:50:05 -0500

    V.0

---
+++ CODE/EDICOMPUCyberManagementsystem/src/ec/edu/espe/EDICOMPUCMS/view/EDICOMPUCyberManagementSystem.java
@@ -0,0 +1,8 @@
+
+package ec.edu.espe.EDICOMPUCMS.view;
+
+public class EDICOMPUCyberManagementSystem {
+    public static void main(String[ ] args){
+        System.out.println("Hello wdgfgorld");
+    }
+}
