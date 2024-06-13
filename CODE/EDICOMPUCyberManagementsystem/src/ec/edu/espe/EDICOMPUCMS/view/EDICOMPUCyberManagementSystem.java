bd9c2a16a40d9c51d7d16d2a530e3a43d2f91a3e
Jeancarlo Javier Santi Cruz <sjeancarlo.05jjsc@gmail.com>
2024-06-12 22:12:45 -0500

    login V1.0

--- CODE/EDICOMPUCyberManagementsystem/src/ec/edu/espe/EDICOMPUCMS/view/EDICOMPUCyberManagementSystem.java
+++ CODE/EDICOMPUCyberManagementsystem/src/ec/edu/espe/EDICOMPUCMS/view/EDICOMPUCyberManagementSystem.java
@@ -2,11 +2,25 @@
 package ec.edu.espe.EDICOMPUCMS.view;
 
 import ec.edu.espe.EDICOMPUCMS.model.Login;
+import ec.edu.espe.EDICOMPUCMS.model.Users;
 
 public class EDICOMPUCyberManagementSystem {
     public static void main(String[ ] args){
         
-        System.out.println("Welcome to the system");
+        Users[] users ={
+          new Users("Jeancarlo","jean2005"),
+          new Users("Andrea,","andrea2005")  
+        };
         
+        Login login = new Login(users);
+                  if(login.showLoginMenu()){
+                      showMainMenu();
+                  } else{
+                      System.out.println("exiting the system");
+                  }     
+    }
+    
+    public static void showMainMenu(){
+         System.out.println("Welcome to the system");
     }
 }
