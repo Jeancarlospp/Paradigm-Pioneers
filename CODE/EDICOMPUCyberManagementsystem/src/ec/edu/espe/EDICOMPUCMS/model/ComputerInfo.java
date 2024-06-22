349fb6dcb5a6ec745ebbfe67d1dd61420ac6f0e0
Jeancarlo Javier Santi Cruz <sjeancarlo.05jjsc@gmail.com>
2024-06-21 22:53:50 -0500

    Errores corregidos

---
+++ CODE/EDICOMPUCyberManagementsystem/src/ec/edu/espe/EDICOMPUCMS/model/ComputerInfo.java
@@ -0,0 +1,19 @@
+
+package ec.edu.espe.EDICOMPUCMS.model;
+
+
+public class ComputerInfo {
+    private int id;
+
+    public ComputerInfo(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
