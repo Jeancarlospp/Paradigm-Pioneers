5301fa0d501bcc744f9ff09db2f0aed5c42e2f52
Jeancarlo Javier Santi Cruz <sjeancarlo.05jjsc@gmail.com>
2024-06-24 13:20:31 -0500

    reemplazo computers por computer rental

--- CODE/EDICOMPUCyberManagementsystem/src/ec/edu/espe/EDICOMPUCMS/controller/MainMenu.java
+++ CODE/EDICOMPUCyberManagementsystem/src/ec/edu/espe/EDICOMPUCMS/controller/MainMenu.java
@@ -16,12 +16,11 @@ public class MainMenu {
             System.out.println("\n========== Welcome to the system ==========");
             System.out.println("\tSelect the option you want to perform ");
             System.out.println("1. Customers");
-            System.out.println("2. Computers");
-            System.out.println("3. Computer rental");
-            System.out.println("4. Computer reservation");
-            System.out.println("5. Financial report");
+            System.out.println("2. Computer rental");
+            System.out.println("3. Computer reservation");
+            System.out.println("4. Financial report");
+            System.out.println("5. Exit ");
 
-            System.out.println("6. Exit");
             System.out.print("Select an option: ");
 
             int option = scanner.nextInt();
@@ -36,20 +35,16 @@ public class MainMenu {
                     ComputerMenu.handleComputers();
                     break;
                 case 3:
-                    System.out.println("Computer rental");
-                    // Call method or class for Computer rental
+                    System.out.println("Computer reservation");
+                     // Call method or class for Computer reservation
                     break;
                 case 4:
-                    System.out.println("Computer reservation");
-                    // Call method or class for Computer reservation
-                    break;
-                case 5:
                     System.out.println("Financial report");
                     // Call method or class for Financial report
                     break;
-                case 6:
+                case 5:
                     System.out.println("Exiting the system.");
-
+                    
                     return;
                 default:
                     System.out.println("Invalid option, please try again.");
