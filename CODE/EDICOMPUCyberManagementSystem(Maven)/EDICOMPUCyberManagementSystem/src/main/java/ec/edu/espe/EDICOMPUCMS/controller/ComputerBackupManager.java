bb5a4043a58c58721a5e9718182583e058fc0484
Leidy04S <leidysaraguro691@gmail.com>
2024-07-31 20:56:11 -0500

    I unified the methods to simplify the code.

--- CODE/EDICOMPUCyberManagementSystem(Maven)/EDICOMPUCyberManagementSystem/src/main/java/ec/edu/espe/EDICOMPUCMS/controller/ComputerBackupManager.java
+++ CODE/EDICOMPUCyberManagementSystem(Maven)/EDICOMPUCyberManagementSystem/src/main/java/ec/edu/espe/EDICOMPUCMS/controller/ComputerBackupManager.java
@@ -1,7 +1,7 @@
 package ec.edu.espe.EDICOMPUCMS.controller;
 /**
  *
- * @author Sjean
+ * @author Leidy
  */
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -16,22 +16,25 @@ public class ComputerBackupManager {
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public static void backupComputerStatuses() {
-        Map<String, String> computerStatuses = generateComputerStatuses();
-        saveComputerStatuses(computerStatuses);
+        Map<String, String> computerStatuses = createStatuses();
+        writeStatusesToFile(computerStatuses);
     }
 
-    private static Map<String, String> generateComputerStatuses() {
-        Map<String, String> computerStatuses = new LinkedHashMap<>();
+    private static Map<String, String> createStatuses() {
+        return generateStatuses();
+    }
+    
+    private static Map<String, String> generateStatuses() {
+        Map<String, String> statuses = new LinkedHashMap<>();
         for (int i = 1; i <= 10; i++) {
-            computerStatuses.put("computer " + i, "available");
+            statuses.put("computer " + i, "available");
         }
-        return computerStatuses;
+        return statuses;
     }
 
-    private static void saveComputerStatuses(Map<String, String> computerStatuses) {
+    private static void writeStatusesToFile(Map<String, String> statuses) {
         try (FileWriter writer = new FileWriter(FILE_NAME)) {
-            gson.toJson(computerStatuses, writer);
-            // Omitir el mensaje de respaldo completado
+            gson.toJson(statuses, writer);
         } catch (IOException e) {
             System.out.println("Error saving computer statuses to file " + FILE_NAME + ": " + e.getMessage());
         }
