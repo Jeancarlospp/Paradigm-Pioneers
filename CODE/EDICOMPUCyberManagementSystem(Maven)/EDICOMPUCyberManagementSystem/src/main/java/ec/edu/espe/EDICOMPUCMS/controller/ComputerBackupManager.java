de773ff4efa2f241c463bf4177ff9ce53883f307
Leidy04S <leidysaraguro691@gmail.com>
2024-08-18 21:16:04 -0500

    Optimized class by removing redundant methods, adding Logger for error handling, parameterizing file name, and ensuring Map immutability.

--- CODE/EDICOMPUCyberManagementSystem(Maven)/EDICOMPUCyberManagementSystem/src/main/java/ec/edu/espe/EDICOMPUCMS/controller/ComputerBackupManager.java
+++ CODE/EDICOMPUCyberManagementSystem(Maven)/EDICOMPUCyberManagementSystem/src/main/java/ec/edu/espe/EDICOMPUCMS/controller/ComputerBackupManager.java
@@ -4,8 +4,11 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,31 +16,27 @@ import java.util.Map;
  */
 
 public class ComputerBackupManager {
-    private static final String FILE_NAME = "computer_backup.json";
+    private static final Logger LOGGER = Logger.getLogger(ComputerBackupManager.class.getName());
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    public static void backupComputerStatuses() {
-        Map<String, String> computerStatuses = createStatuses();
-        writeStatusesToFile(computerStatuses);
+    public static void backupComputerStatuses(String fileName) {
+        Map<String, String> computerStatuses = generateStatuses();
+        writeStatusesToFile(computerStatuses, fileName);
     }
 
-    private static Map<String, String> createStatuses() {
-        return generateStatuses();
-    }
-    
     private static Map<String, String> generateStatuses() {
         Map<String, String> statuses = new LinkedHashMap<>();
         for (int i = 1; i <= 10; i++) {
             statuses.put("computer " + i, "available");
         }
-        return statuses;
+        return Collections.unmodifiableMap(statuses);
     }
 
-    private static void writeStatusesToFile(Map<String, String> statuses) {
-        try (FileWriter writer = new FileWriter(FILE_NAME)) {
+    private static void writeStatusesToFile(Map<String, String> statuses, String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
             gson.toJson(statuses, writer);
         } catch (IOException e) {
-            System.out.println("Error saving computer statuses to file " + FILE_NAME + ": " + e.getMessage());
+            LOGGER.log(Level.SEVERE, "Error saving computer statuses to file " + fileName, e);
         }
     }
 }
